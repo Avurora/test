fdb673ece1950cf0f4bba680bfd0d6dc250ad1d7
Avurora <58685635+Avurora@users.noreply.github.com>
2019-12-13 02:21:41 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,59 @@
+// JavaScript Document
+
+$(function(){
+    
+    
+    $('#coin-collect-botton').click(function(){
+        $('#coin-collect').slideToggle()
+        
+    });
+    
+    
+    $('#haori-collect-botton').click(function(){
+        $('#haori-collect').slideToggle()
+    });
+    
+    
+    
+//ステップ３の複数階層のクリック
+    $('#trade-botton').click(function(){
+        $('#trade').slideToggle()
+    });
+    
+    
+    $('#donuts').click(function(){
+        $('#donuts-list').slideToggle()
+    });
+    
+    $('#hamburger').click(function(){
+        $('#hamburger-list').slideToggle()
+    });
+    
+    $('#drink').click(function(){
+        $('#drink-list').slideToggle()
+    });
+    
+
+//    ステップ４
+    $('#trade-manner-botton').click(function(){
+        $('#trade-manner').slideToggle()
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+});
