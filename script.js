89799ed1de7fb2110747e5f7c9abca11753607f2
Avurora <58685635+Avurora@users.noreply.github.com>
2019-12-18 23:11:00 +0900

    Update script.js

--- script.js
+++ script.js
@@ -40,13 +40,46 @@ $(function(){
     });
     
     
+ 
+    
+    
+    
+    
+    $(function(){
+    $(window).scroll(function (){
+        $('fadeIn').each(function(){
+            var targetElement = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > targetElement - windowHeight + 200){
+                $(this).css('opacity','1');
+                $(this).css('transform','translateY(0)');
+            }
+        });
+    });
+        
+        
+        
+        
+        
+        
+        
+});
     
     
     
     
     
-    
-    
+   $(function(){
+  $('a[href^="#"]').click(function(){
+    var speed = 500;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+}); 
     
     
     
